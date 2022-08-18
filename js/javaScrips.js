cbfd4354d5d68d47d678c692af8675562ca90f84
FRANCESCA MATTIOLI <francescajuanmattioli1@gmail.com>
2022-08-17 22:02:17 -0300

    Dom y Eventos

--- js/javaScrips.js
+++ js/javaScrips.js
@@ -1,33 +1,71 @@
-//Uso de array
-const articulos = [{id:1, articulo:"Mate", precio:+1400},{id:2, articulo:"Ilustracion", precio:+1500}]
+//Array articulos
+const articulos = [{
+    id: 1,
+    articulo: "Mate",
+    precio: +1400
+}, {
+    id: 2,
+    articulo: "Ilustracion",
+    precio: +1500
+}, {
+    id: 3,
+    articulo: "Print",
+    precio: +1200
+}, {
+    id: 4,
+    articulo: "Cuadro",
+    precio: +2000
+}, {
+    id: 5,
+    articulo: "MiniBastidor",
+    precio: +1900
+}]
 
-//Uso de Ciclo y Condicional
-let producto
-let precioMate = +1400
-let precioIlus = +1500
-
-do{
-    producto = prompt("Ingrese el nombre del producto que desea agregar al carrito (mate/ilustración), escriba 'Salir' para salir");
-
-if (producto == "mate"){
-    alert ("Ingresaste un mate")
-    console.log(articulos.slice(0,1))
-}else if  (producto == "ilustracion"){
-    alert ("Ingresaste una ilustración")
-    console.log(articulos.slice(1,2))
-}else if (producto== "salir"){
-    alert("Nos vemos!")
-}else {
-    alert ("Ingrese un valor válido (mate / ilustracion / salir)")
+//Funcion botones comprar
+let btnMate = document.getElementById("mateBtn")
+btnMate.addEventListener("click", respuestaCompraMate)
+function respuestaCompraMate() {
+    console.log(articulos[0]);
 }
 
-}while(producto!="salir") 
-
-//Sumo si se agrega el articulo
-const suma = (a, b) => (a + b)
-
-let total = suma (precioIlus, precioMate)
-
-if (total !== 0){
-    console.log(total);
+let btnIlus = document.getElementById("ilusBtn")
+btnIlus.addEventListener("click", respuestaCompraIlus)
+function respuestaCompraIlus() {
+    console.log(articulos[1]);
 }
+
+let btnPrint = document.getElementById("printBtn")
+btnPrint.addEventListener("click", respuestaCompraPrint)
+function respuestaCompraPrint() {
+    console.log(articulos[2]);
+}
+
+let btnCuadro = document.getElementById("cuadroBtn")
+btnCuadro.addEventListener("click", respuestaCompraCuadro)
+function respuestaCompraCuadro() {
+    console.log(articulos[3]);
+}
+
+let btnMiniB = document.getElementById("miniBBtn")
+btnMiniB.addEventListener("click", respuestaCompraMiniB)
+function respuestaCompraMiniB() {
+    console.log(articulos[4]);
+}
+
+//Funcion carrito
+let btnCarrito = document.getElementById("carrito")
+btnCarrito.addEventListener("click", respuestaCarrito)
+function respuestaCarrito() {
+    console.log("Tu carrito: ");
+}
+
+//Suponiendo la compra de 1 ilustracion y 1 print:
+/*const suma = (a, b) => (a + b)
+
+let totalCarrito = suma(articulos[1].precio, articulos[2].precio)
+
+if (totalCarrito !== 0) {
+    console.log(totalCarrito);
+}*/
+
+//No se como hacer que al apretar el carrito sume los pedidos que se crean en la consola. Y que luego muestre el totalCarrito.
